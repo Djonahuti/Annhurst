d99cd6e6a37d6aeda3ed9438155f67eb1b588703
Djonahuti <djonah04@gmail.com>
2025-10-08 09:32:26 +0100

    commit

--- src/app/(root)/contact/page.tsx
+++ src/app/(root)/contact/page.tsx
@@ -412,7 +412,7 @@ export default function ContactPage() {
       <div className="bg-gray-50 dark:bg-gray-900 py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center mb-16">
-            <h2 className="text-base font-semibold leading-7 text-primary dark:text-primary-light">Find Us</h2>
+            <h2 className="text-base font-semibold leading-7 text-primary dark:text-primary-light">{page.body_sub_heading3}</h2>
             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-300 sm:text-4xl">
             {page.body_heading3}
             </p>
